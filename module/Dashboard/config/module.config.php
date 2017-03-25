67790ca205d77515b81466b90b54b90caac13251
linuser <linuser@unix.local>
2017-03-25 15:49:33 +0100

    Module Dashboard added

---
+++ module/Dashboard/config/module.config.php
@@ -0,0 +1,33 @@
+<?php
+namespace Dashboard;
+
+use Zend\Router\Http\Literal;
+// use Zend\Router\Http\Segment;
+use Zend\ServiceManager\Factory\InvokableFactory;
+
+return [
+    'router' => [
+        'routes' => [
+            'home' => [
+                'type' => Literal::class,
+                'options' => [
+                    'route'    => '/',
+                    'defaults' => [
+                        'controller' => Controller\IndexController::class,
+                        'action'     => 'index',
+                    ],
+                ],
+            ],
+        ],
+    ],
+    'controllers' => [
+        'factories' => [
+            Controller\IndexController::class => InvokableFactory::class,
+        ],
+    ],
+    'view_manager' => [
+        'template_path_stack' => [
+            __DIR__ . '/../view',
+        ],
+    ],
+];
